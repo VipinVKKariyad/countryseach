4b1b93693ebbeda0062c85960c8682a985cac419
VipinVKKariyad <yoursvipinalways@gmail.com>
2025-07-15 00:41:03 -0700

    Formatting

--- src/countries.jsx
+++ src/countries.jsx
@@ -10,50 +10,51 @@ function Countries() {
   const [countries, setCountries] = useState([])
   const [filteredCountries, setFilteredCountries] = useState([])
   const [filter, setFilter] = useState("")
-useEffect((
+  useEffect((
 
- 
-)=>{ fetch("https://countries-search-data-prod-812920491762.asia-south1.run.app/countries")
-  .then(res=>res.json())
-  .then(data=>
-    {
-    setCountries(data)
-    setFilteredCountries(data);
-    //  console.log(countries,filteredCountries)
+
+  ) => {
+    fetch("https://countries-search-data-prod-812920491762.asia-south1.run.app/countries")
+    .then(res => res.json())
+    .then(data => {
+      setCountries(data)
+      setFilteredCountries(data);
+      //  console.log(countries,filteredCountries)
     })
-},
-[]);
-  
-
-
-useEffect(()=>{
-  console.log("filterchanged",filter)
-   const FilteredCountries = countries.filter((item)=> item.common.toLowerCase().includes(filter.toLowerCase()))
-  // console.log(countries)
-  setFilteredCountries(FilteredCountries)
-
-},[filter])
+  },
+    []);
 
 
 
-const handleOnchange=(e)=>{
-  (setFilter(e.target.value)
+  useEffect(() => {
+    console.log("filterchanged", filter)
+    const FilteredCountries = countries.filter((item) => item.common.toLowerCase().includes(filter.toLowerCase()))
+    // console.log(countries)
+    setFilteredCountries(FilteredCountries)
 
-)}
+  }, [filter])
+
+
+
+  const handleOnchange = (e) => {
+    (setFilter(e.target.value)
+
+    )
+  }
 
 
   return (
     <>
-      <div style={{display:"flex" , justifyContent:"center",alignItems:"center", flexDirection:"column"}}>
-      <h1> Countries</h1>
-       <input type='text' placeholder="Search for countries..."
- onChange={(e) => handleOnchange(e)} style={{maxWidth:"400px" , width:"100%"}}/>
-      <div style={{display:"flex", flexWrap:"wrap", margin:"10px"}}>
+      <div style={{ display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column" }}>
+        <h1> Countries</h1>
+        <input type='text' placeholder="Search for countries..."
+          onChange={(e) => handleOnchange(e)} style={{ maxWidth: "400px", width: "100%" }} />
+        <div style={{ display: "flex", flexWrap: "wrap", margin: "10px" }}>
 
-    {filteredCountries.map((item, index) => (
-          <CountriesCard item={item} key={index}/>
-        ))}
-</div>
+          {filteredCountries.map((item, index) => (
+            <CountriesCard item={item} key={index} />
+          ))}
+        </div>
       </div>
     </>
   )
