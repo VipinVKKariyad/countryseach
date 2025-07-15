485b76bbb37316ee3a412853c62d600d8d6769ab
VipinVKKariyad <yoursvipinalways@gmail.com>
2025-07-15 00:34:28 -0700

    Countries name modified

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Countries from './Countries.jsx'
+import Countries from './countries.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
